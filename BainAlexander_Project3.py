637ebd2117b345984150b6df363d999392d81a2f
abain42 <alex.bain208@gmail.com>
2024-11-22 21:10:05 -0500

    start
    Not much so far just created the function of the ODE's. So far the majority, of the time has been spent trying to understand the math, and also learn how to use solve ivp

--- BainAlexander_Project3.py
+++ BainAlexander_Project3.py
@@ -3,7 +3,17 @@
 #part 1:
 import numpy as np
 from scipy import integrate
-mu = 2
 
+integrate.solve_ivp()
+
+mu = 2
+def Ode(r,y,rhoval):
+    rho, m = y
+    if rho <= 0:
+        return [0,0]
+    
+    x = rho**(1/3)
+    gammaX = x**2/np.sqrt(3*(1+x**2))
+    drho_dr = -m*rho / (gammaX * r**2)
+    dm_dr = r**2 * rho
 pc = np.linspace(0.1,2.5e6,10)
-print(pc)
